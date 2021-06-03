2fe5f2e6b1ecab55fa13ff5a5eac26e39768b7b8
Sebushka89 <sebastian.rizzo@hotmail.it>
2021-06-03 19:36:15 +0200

    aggiunto se utente sbaglia a scrivere in minuscolo o tutto maiuscolo

--- js/script.js
+++ js/script.js
@@ -1,9 +1,11 @@
 var cognomeUtente = prompt('Inserire qua il proprio cognome');
 var cognomi = [ 'Bianchi', 'Neri', 'Rossi', 'Verdi', 'Gialli'];
+cognomeUtente = cognomeUtente.charAt(0).toUpperCase() + cognomeUtente.slice(1).toLowerCase();
 cognomi.push(cognomeUtente);
 cognomi.sort();
+console.log(cognomeUtente);
 
-document.getElementById('lista-cognomi').innerHTML += 'La lista dei cognomi è:'+ '  ' + cognomi
+document.getElementById('lista-cognomi').innerHTML += 'La lista dei cognomi è :' + '  ' +  cognomi 
 
 var posizione = cognomi.indexOf(cognomeUtente) + 1 ;
 
